4ab9802f4137a53865a63d7961a4171e9c6abab1
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-08-31 14:00:01 -0300

    Add files via upload

---
+++ Selenium/selenium.py
@@ -0,0 +1,12 @@
+
+from selenium import webdriver
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.chrome.service import Service
+
+servico = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service=servico)
+#site = webdriver.Chrome(service=servico)
+#paginas = webdriver.Chrome(service=servico)
